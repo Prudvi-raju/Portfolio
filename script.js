8e09118bb58c069daae928bbbe4122a3ba6570a5
PrudviRaju <113966095+Prudvi-raju@users.noreply.github.com>
2024-08-20 14:31:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+window.addEventListener("scroll", function () {
+  let scrool = document.querySelector("#scrool-up");
+  scrool.classList.toggle("srcl", window.scrollY > 600);
+});
